31ad5159ccadb75053a823e7703d18bc08aec948
d-kimsuon <dev.kimuson@gmail.com>
2024-12-28 16:44:30 +0900

    add mcps build

---
+++ scripts/build_mcps.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+function npm_install_and_build() {
+  local dir=$1
+
+  pushd ./mcps/$dir
+  npm ci
+  npm run build
+  popd
+}
+
+npm_install_and_build "command-executor-mcp"
