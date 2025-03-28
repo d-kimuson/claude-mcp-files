86a3126c4c6e99386521750ccf47471f0d43dbd1
d-kimsuon <dev.kimuson@gmail.com>
2025-03-29 02:25:17 +0900

    お掃除

--- scripts/generate-config/declare.ts
+++ scripts/generate-config/declare.ts
@@ -1,8 +1,8 @@
+import { resolve } from "node:path"
 import { createGenerateConfig, defineMcpServer, dirs } from "./helper"
 import * as v from "valibot"
 
 const envSchemas = {
-  MCP_BRAVE_API_KEY: v.string(),
   MCP_ESA_API_KEY: v.string(),
   MCP_ESA_DEFAULT_TEAM: v.string(),
   MCP_EXA_API_KEY: v.string(),
@@ -27,6 +27,66 @@ const envSchemas = {
   MCP_SLACK_TEAM_ID: v.string(),
 } as const
 
+/**
+ * Thinking
+ */
+
+const sequentialThinkingServer = defineMcpServer(
+  "sequential-thinking",
+  v.object({
+    MCP_NPX_PATH: envSchemas.MCP_NPX_PATH,
+  }),
+  ({ env }) => ({
+    command: env.MCP_NPX_PATH,
+    args: ["-y", "@modelcontextprotocol/server-sequential-thinking"],
+  })
+)
+
+const thinkServer = defineMcpServer(
+  "think",
+  v.object({
+    MCP_NODE_PATH: envSchemas.MCP_NODE_PATH,
+  }),
+  ({ env }) => ({
+    command: env.MCP_NODE_PATH,
+    args: [resolve(process.cwd(), "packages", "think", "dist", "index.js")],
+  })
+)
+
+/**
+ * Research
+ */
+
+const exaMcpServer = defineMcpServer(
+  "exa",
+  v.object({
+    MCP_NPX_PATH: envSchemas.MCP_NPX_PATH,
+    MCP_EXA_API_KEY: envSchemas.MCP_EXA_API_KEY,
+  }),
+  ({ env }) => ({
+    command: env.MCP_NPX_PATH,
+    args: ["-y", "exa-mcp-server"],
+    env: {
+      EXA_API_KEY: env.MCP_EXA_API_KEY,
+    },
+  })
+)
+
+const fetchServer = defineMcpServer(
+  "fetch",
+  v.object({
+    MCP_UVX_PATH: envSchemas.MCP_UVX_PATH,
+  }),
+  ({ env }) => ({
+    command: env.MCP_UVX_PATH,
+    args: ["mcp-server-fetch"],
+  })
+)
+
+/**
+ * Development
+ */
+
 const filesystemServer = defineMcpServer(
   "filesystem",
   v.object({
@@ -45,55 +105,14 @@ const filesystemServer = defineMcpServer(
   })
 )
 
-const braveSearchServer = defineMcpServer(
-  "brave-search",
-  v.object({
-    MCP_NPX_PATH: envSchemas.MCP_NPX_PATH,
-    MCP_BRAVE_API_KEY: envSchemas.MCP_BRAVE_API_KEY,
-  }),
-  ({ env }) => ({
-    command: env.MCP_NPX_PATH,
-    args: ["-y", "@modelcontextprotocol/server-brave-search"],
-    env: {
-      BRAVE_API_KEY: env.MCP_BRAVE_API_KEY,
-    },
-  })
-)
-
-const webResearchServer = defineMcpServer(
-  "webresearch",
+const mcpServerCommandsServer = defineMcpServer(
+  "mcp-server-commands",
   v.object({
     MCP_NPX_PATH: envSchemas.MCP_NPX_PATH,
   }),
   ({ env }) => ({
     command: env.MCP_NPX_PATH,
-    args: ["-y", "@mzxrai/mcp-webresearch"],
-  })
-)
-
-const exaMcpServer = defineMcpServer(
-  "exa",
-  v.object({
-    MCP_NPX_PATH: envSchemas.MCP_NPX_PATH,
-    MCP_EXA_API_KEY: envSchemas.MCP_EXA_API_KEY,
-  }),
-  ({ env }) => ({
-    command: env.MCP_NPX_PATH,
-    args: ["-y", "exa-mcp-server"],
-    env: {
-      EXA_API_KEY: env.MCP_EXA_API_KEY,
-    },
-  })
-)
-
-const sequentialThinkingServer = defineMcpServer(
-  "sequential-thinking",
-  v.object({
-    MCP_NPX_PATH: envSchemas.MCP_NPX_PATH,
-  }),
-  ({ env }) => ({
-    command: env.MCP_NPX_PATH,
-    args: ["-y", "@modelcontextprotocol/server-sequential-thinking"],
+    args: ["mcp-server-commands"],
   })
 )
 
@@ -112,16 +131,9 @@ const githubServer = defineMcpServer(
   })
 )
 
-const fetchServer = defineMcpServer(
-  "fetch",
-  v.object({
-    MCP_UVX_PATH: envSchemas.MCP_UVX_PATH,
-  }),
-  ({ env }) => ({
-    command: env.MCP_UVX_PATH,
-    args: ["mcp-server-fetch"],
-  })
-)
+/**
+ * SaaS
+ */
 
 const slackServer = defineMcpServer(
   "slack",
@@ -137,39 +149,6 @@ const slackServer = defineMcpServer(
       SLACK_BOT_TOKEN: env.MCP_SLACK_BOT_TOKEN,
       SLACK_TEAM_ID: env.MCP_SLACK_TEAM_ID,
     },
-  })
-)
-
-const puppeteerServer = defineMcpServer(
-  "puppeteer",
-  v.object({
-    MCP_NPX_PATH: envSchemas.MCP_NPX_PATH,
-  }),
-  ({ env }) => ({
-    command: env.MCP_NPX_PATH,
-    args: ["-y", "@modelcontextprotocol/server-puppeteer"],
-  })
-)
-
-const timeServer = defineMcpServer(
-  "time",
-  v.object({
-    MCP_UVX_PATH: envSchemas.MCP_UVX_PATH,
-  }),
-  ({ env }) => ({
-    command: env.MCP_UVX_PATH,
-    args: ["mcp-server-time"],
-  })
-)
-
-const mcpServerCommandsServer = defineMcpServer(
-  "mcp-server-commands",
-  v.object({
-    MCP_NPX_PATH: envSchemas.MCP_NPX_PATH,
-  }),
-  ({ env }) => ({
-    command: env.MCP_NPX_PATH,
-    args: ["mcp-server-commands"],
   })
 )
 
@@ -205,34 +184,40 @@ const figmaServer = defineMcpServer(
   })
 )
 
-const claudeCodeServer = defineMcpServer(
-  "claude-code",
+/**
+ * Others
+ */
+
+const timeServer = defineMcpServer(
+  "time",
   v.object({
-    MCP_CLAUDE_PATH: envSchemas.MCP_CLAUDE_PATH,
+    MCP_UVX_PATH: envSchemas.MCP_UVX_PATH,
   }),
   ({ env }) => ({
-    command: env.MCP_CLAUDE_PATH,
-    args: ["mcp", "serve"],
+    command: env.MCP_UVX_PATH,
+    args: ["mcp-server-time"],
   })
 )
 
 const mcpServers = [
-  filesystemServer,
-  braveSearchServer,
-  webResearchServer,
-  exaMcpServer,
+  // thinking
+  thinkServer,
   sequentialThinkingServer,
-  githubServer,
+
+  // research
+  exaMcpServer,
   fetchServer,
-  slackServer,
-  puppeteerServer,
-  // 動かないので ignore
-  // reason: zoneinfo._common.ZoneInfoNotFoundError: 'No time zone found with key JST'
-  timeServer,
+
+  // development
+  filesystemServer,
   mcpServerCommandsServer,
+  githubServer,
+  slackServer,
   esaServer,
   figmaServer,
-  claudeCodeServer,
+
+  // others
+  timeServer,
 ] as const
 
 export type McpServerName = (typeof mcpServers)[number]["name"]
